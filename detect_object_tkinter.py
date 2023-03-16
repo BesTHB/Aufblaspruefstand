795a9675ce25ebfe250591fb9e9bf1eab84af667
s.becker <s.becker@th-bingen.de>
2023-03-16 08:37:39 +0100

    + Druckauswertung und Plots in GUI (tkinter) hinzugefuegt.

--- detect_object_tkinter.py
+++ detect_object_tkinter.py
@@ -2,6 +2,12 @@ import cv2
 import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
+import serial
+from datetime import datetime
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+import matplotlib.pyplot as plt
 
 # src: https://www.youtube.com/watch?v=lbgl2u6KrDU
 # src: https://www.youtube.com/watch?v=UlM2bpqo_o0
@@ -11,6 +17,21 @@ from PIL import Image, ImageTk
 
 class Application:
     def __init__(self):
+        # connect with serial port (Pi Pico)
+        self.port = 'COM6'
+        self.ser = serial.Serial(self.port, 9600)
+        
+        # set values for converting the serial input to mbar
+        self.v_in = 3.3                   # input voltage in V
+        self.v_0  = self.v_in/10          # voltage at  0psi (10% of v_in)
+        self.v_10 = self.v_in-self.v_0    # voltage at 10psi (90% of v_in)
+        self.time_start = datetime.now()  # store start time
+        
+        # initialize lists for time, pressure and diameter
+        self.time = []
+        self.pressure = []
+        self.diameter = []
+    
         # connect with webcam
         # (iterate through numbers in order to find the correct webcam, if multiple are available)
         self.cap = cv2.VideoCapture(0)
@@ -36,9 +57,28 @@ class Application:
         # init image panel
         self.panel = tk.Label(self.win)
         self.panel.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        
+        # initialize plot canvas
+        self.fig, self.axs = plt.subplots(3, 1, tight_layout=True, figsize=(5, 6))
+        self.line_p_over_t, = self.axs[0].plot(self.time, self.pressure)
+        self.line_d_over_t, = self.axs[1].plot(self.time, self.diameter)
+        self.line_p_over_d, = self.axs[2].plot(self.diameter, self.pressure)
+        self.axs[0].set_xlabel('Versuchslaufzeit / s')
+        self.axs[0].set_ylabel('Druck / mbar')
+        self.axs[0].set_ylim(0, 100)
+        self.axs[1].set_xlabel('Versuchslaufzeit / s')
+        self.axs[1].set_ylabel('Durchmesser / mm')
+        self.axs[1].set_ylim(0, 250)
+        self.axs[2].set_xlabel('Durchmesser / mm')
+        self.axs[2].set_ylabel('Druck / mbar')
+        self.axs[2].set_xlim(0, 250)
+        self.axs[2].set_ylim(0, 100)
+        self.plot_canvas = FigureCanvasTkAgg(self.fig, master=self.win)  # A tk.DrawingArea.
+        self.plot_canvas.draw()
+        self.plot_canvas.get_tk_widget().grid(row=0, column=2, rowspan=5, padx=10, pady=10)
 
         # create sliders for hsv
-        self.hsv_min_max = [[60, 90], [95, 255], [50, 255]]
+        self.hsv_min_max = [[60, 88], [80, 255], [90, 255]]
         self.h_min = tk.IntVar()
         self.h_max = tk.IntVar()
         self.s_min = tk.IntVar()
@@ -78,8 +118,8 @@ class Application:
         self.btn_switch_img = tk.Button(self.win, text='switch img', command=self.switch_img)
         self.btn_switch_img.grid(row=5, column=1, sticky='nsew', padx=10, pady=10)
 
-        # start video loop
-        self.video_loop()
+        # start measurement loop
+        self.measurement_loop()
 
     def reset_values(self):
         self.slider_h_min.set(self.hsv_min_max[0][0])
@@ -88,7 +128,7 @@ class Application:
         self.slider_h_max.set(self.hsv_min_max[0][1])
         self.slider_s_max.set(self.hsv_min_max[1][1])
         self.slider_v_max.set(self.hsv_min_max[2][1])
-        self.slider_minArea.set(400)
+        self.slider_minArea.set(2000)
 
     def switch_img(self):
         if self.img_displayed == 0:
@@ -96,7 +136,29 @@ class Application:
         elif self.img_displayed == 1:
             self.img_displayed = 0
 
-    def video_loop(self):
+    def measurement_loop(self):
+        # read 16bit serial value, convert to string as UTF-8, rstrip newline-character and ...
+        try:
+            # convert to int
+            sensorVal = int(str(self.ser.readline(), 'UTF-8').rstrip('\n'))
+        except ValueError:
+            # convert to float
+            sensorVal = float(str(self.ser.readline(), 'UTF-8').rstrip('\n'))
+        
+        # convert 16bit integer to Volt
+        voltage = sensorVal*self.v_in/(2**16)
+        
+        # convert Volt to psi (0.1*v_in == v_0 == 0psi ; 0.9*v_in == v_10 == 10psi)
+        m = 10/(self.v_10-self.v_0)
+        b = -self.v_0*m
+        p_psi = m*voltage + b
+
+        # convert psi to mbar
+        p_mbar = 68.9476*p_psi
+
+        # get time-delta between start time and now and convert it to seconds (float)
+        dt = (datetime.now() - self.time_start).total_seconds()
+    
         # get current webcam imgage
         ret, self.imgs[0] = self.cap.read()
         if ret:
@@ -148,6 +210,23 @@ class Application:
                     cv2.putText(self.imgs[self.img_displayed], f'[pixel] width: {w:.1f}, height: {h:.1f}', (int(x), int(y)+15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
                     try:
                         cv2.putText(self.imgs[self.img_displayed], f'[mm] width: {w/pixel_mm_ratio:.2f}, height: {h/pixel_mm_ratio:.2f}', (int(x), int(y)+30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+                                        
+                        # append measurements to respective lists
+                        self.time.append(dt)
+                        self.pressure.append(p_mbar)
+                        self.diameter.append(w/pixel_mm_ratio)
+                        
+                        # update plot
+                        self.line_p_over_t.set_data(self.time, self.pressure)
+                        self.axs[0].set_xlim(0, max(self.time))
+                        self.line_d_over_t.set_data(self.time, self.diameter)
+                        self.axs[1].set_xlim(0, max(self.time))
+                        self.line_p_over_d.set_data(self.diameter, self.pressure)
+                        #self.axs[0].plot(self.time, self.pressure, c='b')
+                        #self.axs[1].plot(self.time, self.diameter, c='b')
+                        #self.axs[2].plot(self.diameter, self.pressure, c='b')
+                        self.plot_canvas.draw()
+                
                     except UnboundLocalError:
                         # if pixel_mm_ratio is not present, pass
                         pass
@@ -159,7 +238,7 @@ class Application:
             self.panel.imgtk = imgtk
             self.panel.config(image=imgtk)
 
-        self.win.after(30, self.video_loop)   # call same function after 30 ms
+        self.win.after(30, self.measurement_loop)   # call same function after 30 ms
 
     def calc_aruco_widths(self, c):
         x0, y0 = c[0]
@@ -171,7 +250,8 @@ class Application:
         return wx, wy
 
     def destructor(self):
-        # close connection to webcam and destroy webcam window
+        # close plot, close connection to webcam and destroy webcam window
+        plt.close()
         self.win.destroy()
         self.cap.release()
         cv2.destroyAllWindows()
