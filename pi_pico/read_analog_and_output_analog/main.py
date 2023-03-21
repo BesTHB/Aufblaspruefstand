f34fc3c8253a792aa82ccbb34b7aefedfa88e149
s.becker <s.becker@th-bingen.de>
2023-03-21 11:29:48 +0100

    * Druckwerte (analoges Signal) mit 5Hz statt 2Hz senden.

--- pi_pico/read_analog_and_output_analog/main.py
+++ pi_pico/read_analog_and_output_analog/main.py
@@ -1,6 +1,6 @@
 # This is a MicroPython script for the Pi Pico.
 # It reads an analog input on GPIO26 (analog pin A0),
-# prints it and sleeps 0.5s until repeating.
+# prints it and sleeps 0.2s until repeating.
 
 from machine import ADC
 from utime import sleep_ms
@@ -9,4 +9,4 @@ sensor = ADC(26)   # GP26 -> A0
 
 while True:
     print(sensor.read_u16())    # read analog input (will come as an unsigned 16-bit integer, which ranges from 0-65535 !)
-    sleep_ms(500)               # sleep 0.5s
+    sleep_ms(200)               # sleep 0.2s
